8fc550a21b437df758fbd8fa56e5f9d165bef616
bhartisuryaprakash7-crypto <bhartisuryaprakash7@gmail.com>
2026-06-22 13:16:36 +0530

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: '/3D_Portfolio/',
+  base: '/3d_Portfolio/',
 
   build: {
     outDir: 'dist',
